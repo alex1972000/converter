16d90fb9ff7ce045be6bf93c1f4ac04281c9febf
alex1972000 <alex.gajdos@upjs.sk>
2022-02-20 22:52:46 +0100

    Initial commit2

--- src/main/java/aardwark/zadanie/celsiumtofafahrenheit/services/ConverterServiceImpl.java
+++ src/main/java/aardwark/zadanie/celsiumtofafahrenheit/services/ConverterServiceImpl.java
@@ -7,17 +7,23 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static java.lang.Double.NaN;
+
 @Service("converterService")
 @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
 public class ConverterServiceImpl implements ConverterService{
 
     @Override
     public Double convert(String type, double value) {
-        if("FahrenheitToCelsium".equals(type)){
-            return ConversionConstants.FahrenheitToCelsium.apply(value);
-        }else if("CelsiumToFahrenheit".equals(type)){
-            return ConversionConstants.CelsiumToFahrenheit.apply(value);
+        try {
+            if ("FahrenheitToCelsium".equals(type)) {
+                return ConversionConstants.FahrenheitToCelsium.apply(value);
+            } else if ("CelsiumToFahrenheit".equals(type)) {
+                return ConversionConstants.CelsiumToFahrenheit.apply(value);
+            }
+        } catch (ConversionException e) {
+            throw new ConversionException("Convertion not sucessful", e);
         }
-        throw new ConversionException("Convertion not sucessful");
+        return NaN;
     }
 }
